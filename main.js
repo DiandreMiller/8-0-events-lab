e5bea9809b93a478b5d5b2640e7dba271ea10992
Diandre Miller <millerdiandre@gmail.com>
2022-12-18 15:43:44 -0500

    Completed Events Labs

--- main.js
+++ main.js
@@ -7,3 +7,38 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+let currentColorDiv = document.getElementById('current-color');
+let colors = document.querySelectorAll('.color');
+
+colors.forEach(color => {
+  color.addEventListener('click', handleClick => {
+     currentColorDiv.style.background = handleClick.target.style.background 
+  } ) 
+
+
+})
+
+let cells = document.querySelectorAll('.cell')
+
+cells.forEach(cell => {
+  cell.addEventListener('click', handleClick => {
+    cell.style.background = currentColorDiv.style.background
+  })
+})
+
+let resetButton = document.querySelector('.reset');
+
+resetButton.addEventListener('click', handleClick => {
+  cells.forEach(cell => {
+    cell.style.background = 'grey'
+  })
+});
+
+let fill = document.querySelector('.fill');
+
+fill.addEventListener('click', handleClick => {
+  cells.forEach(cell => {
+    cell.style.background = currentColorDiv.style.background
+  })
+})
